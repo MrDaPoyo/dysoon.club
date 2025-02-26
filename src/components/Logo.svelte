5b9a4dd03ccf4cc2c57ff100b8907a128c128b83
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2025-02-26 09:53:04 +0000

    not wokeing

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -3,35 +3,14 @@
     import beginRacing from "../assets/images/begin_racing.svg";
     import strips from "../assets/images/strips.svg";
     let props = $props();
-    let beginRacingVisible = true;
 </script>
 
 {#if props.full}
     <div class="full">
-        <img src={logoImage.src} alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 100%; height: auto;" />
-        <img src={strips.src} alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 100%; height: auto;" />
-        <img src={beginRacing.src} alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 100%; height: auto; cursor: pointer;" class:fadeOut={beginRacingVisible} on:click={() => beginRacingVisible = false}  />
-        <style>
-            .fadeOut {
-            animation: fadeOut 1s infinite;
-            }
-
-            @keyframes fadeOut {
-            0% {
-                opacity: 1;
-            }
-            33% {
-                opacity: 0;
-            }
-            66% {
-                opacity: 1;
-            }
-            }
-        </style>
-        <script>
-            let beginRacingVisible = true;
-        </script>
-        <img src={strips.src} alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 100%; height: auto;" />
+        <img src={logoImage.src} alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 40%; width: 100%;" />
+        <img src={strips.src} alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 40%; width: 100%;" />
+        <img src={beginRacing.src} id ="beginRacing" alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 40%; width: 100%;" on:click={navigate('/home')} />
+        <img src={strips.src} alt="DYSOON.CLUB GO GO GOOOO!!!!!" style="max-height: 40%; width: 100%;" />
     </div>
     <style>
         * {
@@ -45,7 +24,6 @@
             justify-content: center;
             align-items: center;
             flex-direction: column;
-            max-height: 80vh;
         }
     </style>
 {:else}
